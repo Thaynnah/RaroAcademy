57ac95100b9c91b378da2e4c22015894f2d40510
Thaynnáh <95942656+Thaynnah@users.noreply.github.com>
2022-03-14 12:53:32 -0300

    Correção da Atividade Prática 03

--- AtividadePratica3.ts
+++ AtividadePratica3.ts
@@ -1,71 +1,68 @@
-// Exercício 1
+// // Exercício 1
 
-const desafio1asRosasNaoFalam = () => {
-    const geraIntervaloAleatório = () => Math.floor(Math.random() * 3 * 1000);
+// const desafio1asRosasNaoFalam = () => {
+//     const geraIntervaloAleatório = () => Math.floor(Math.random() * 3 * 1000);
 
-    const queixoMeAsRosas = (callback) => {
-        setTimeout(() => {
-            console.log('Queixo-me às rosas');
-            callback()
-        }, geraIntervaloAleatório());
-    };
+//     const queixoMeAsRosas = (callback) => {
+//         setTimeout(() => {
+//             console.log('Queixo-me às rosas');
+//             callback()
+//         }, geraIntervaloAleatório());
+//     };
 
-    const masQueBobagem = (callback) => {
-        setTimeout(() => {
-            console.log('Mas que bobagem');
-            callback()
-        }, geraIntervaloAleatório());
-    };
+//     const masQueBobagem = (callback) => {
+//         setTimeout(() => {
+//             console.log('Mas que bobagem');
+//             callback()
+//         }, geraIntervaloAleatório());
+//     };
 
-    const asRosasNaoFalam = (callback) => {
-        setTimeout(() => {
-            console.log('As rosas não falam');
-            callback()
-        }, geraIntervaloAleatório());
-    };
+//     const asRosasNaoFalam = (callback) => {
+//         setTimeout(() => {
+//             console.log('As rosas não falam');
+//             callback()
+//         }, geraIntervaloAleatório());
+//     };
 
-    const simplesmenteAsRosasExalam = (callback) => {
-        setTimeout(() => {
-            console.log('Simplesmente as rosas exalam');
-            callback()
-        }, geraIntervaloAleatório());
-    };
+//     const simplesmenteAsRosasExalam = (callback) => {
+//         setTimeout(() => {
+//             console.log('Simplesmente as rosas exalam');
+//             callback()
+//         }, geraIntervaloAleatório());
+//     };
 
-    const oPerfumeQueRoubamDeTi = (callback) => {
-        setTimeout(() => {
-            console.log('O perfume que roubam de ti, ai');
-            callback()
-        }, geraIntervaloAleatório());
-    };
+//     const oPerfumeQueRoubamDeTi = (callback) => {
+//         setTimeout(() => {
+//             console.log('O perfume que roubam de ti, ai');
+//             callback()
+//         }, geraIntervaloAleatório());
+//     };
 
-    queixoMeAsRosas(
-        () => {
-            masQueBobagem(() => {
-                asRosasNaoFalam(() => {
-                    simplesmenteAsRosasExalam(() => {
-                        oPerfumeQueRoubamDeTi(() => { })
-                    })
-                }
-            )})
-        })
-}
+//     queixoMeAsRosas(
+//         () => {
+//             masQueBobagem(() => {
+//                 asRosasNaoFalam(() => {
+//                     simplesmenteAsRosasExalam(() => {
+//                         oPerfumeQueRoubamDeTi(() => { })
+//                     })
+//                 }
+//             )})
+//         })
+// }
 
-desafio1asRosasNaoFalam();
+// desafio1asRosasNaoFalam();
 
 // Exercício 2
 
-const despertador = (intervalo) => {
+const despertador = (intervalo:Number) => {
     setInterval(() => {
         console.log('vou tocar a cada 1 segundo')
-    }, intervalo);
+    }, 1000);
 }
 
-const relogio = (intervalo) => {
+const relogio = () => {
     const now = new Date();
     setInterval(() => {
     console.log(`${now.getHours()}:${now.getMinutes()}:${now.getSeconds()}`);
-}, intervalo);
+}, 1000);
   };
-
-despertador(1000)
-relogio(1000)
